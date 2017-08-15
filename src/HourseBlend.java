85aa0065b39cdbe243f971383f8d4e0a5a8bf651
2 <30824166+584259039@users.noreply.github.com>
2017-08-15 16:11:57 +0700

    Add Espresso class and Hourseblend class

---
+++ src/HourseBlend.java
@@ -0,0 +1,14 @@
+
+public class HourseBlend extends Beverage {
+	public HourseBlend (){
+		description = "Hourse Blend Coffee";
+	}
+	
+	
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0.89;
+	}
+
+}
